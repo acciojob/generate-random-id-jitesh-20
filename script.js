e91235c3f661cb375cf2a7337e424b33a5bc1569
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-20 15:29:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let res=''
+	let char='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for(let i=0;i<l;i++){
+		const randomIndex = Math.floor(Math.random() * char.length);
+        res += char[randomIndex];
+	}
+	return res;
 }
 
 // Do not change the code below.
